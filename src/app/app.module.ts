e62fceb2286d101e25523b64d34e34370704f765
Yassine <yassine@DEV420.home>
2017-01-15 16:39:20 +0100

    added list of swings with like button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,30 +7,37 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { AngularFireModule } from 'angularfire2';
+import { Storage } from '@ionic/storage';
 
-export const firebaseConfig = {
-
+export const FB_CONFIG = {
+	apiKey:            "AIzaSyCfsMsPgWId2bG7AibwY7CyDccF5mbkC3E",
+	authDomain:        "swing-1484414039704.firebaseapp.com",
+	databaseURL:       "https://swing-1484414039704.firebaseio.com",
+	storageBucket:     "swing-1484414039704.appspot.com",
+	messagingSenderId: "1029159650794"
 };
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations:    [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage
+	],
+	imports:         [
+		IonicModule.forRoot(MyApp),
+		AngularFireModule.initializeApp(FB_CONFIG)
+	],
+	bootstrap:       [IonicApp],
+	entryComponents: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage
+	],
+	providers:       [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,76 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
-import { NavController } from 'ionic-angular';
+import { USERS } from '../../providers/mock-user';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector:    'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	swings: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
+	public searchQuery: string = '';
+	public users               = USERS;
+	public currentUser: any;
 
-  }
+	constructor(public navCtrl: NavController,
+	            public alertCtrl: AlertController,
+	            private fb: AngularFire) {
+		this.swings      = fb.database.list('/swings/swings');
+		this.currentUser = this.users[0];
+		console.log(this.currentUser);
+	}
+
+	onInput() {
+		console.log('onInput');
+	}
+
+	onCancel() {
+		console.log('cancel');
+	}
+
+	goToMap() {
+		console.log('go to map');
+		// navCtrl.push();
+	}
+
+	/**
+	 * toggle the liked swing in currentUser likes array
+	 * @param {string} swingId
+	 */
+	toggleLike(swingId) {
+		let idx = this.currentUser.likes.indexOf(swingId);
+		if (idx > -1) {
+			this.currentUser.likes.splice(idx, 1);
+		} else {
+			this.currentUser.likes.push(swingId);
+		}
+	}
+
+	/**
+	 * return true is swing has been liked by current user
+	 * @param {string} swingId
+	 * @returns {boolean}
+	 */
+	hasLiked(swingId) {
+		return this.currentUser.likes.indexOf(swingId) > -1;
+	}
+
+	/**
+	 * like or remove like of clicked swing
+	 * @param swing
+	 */
+	likeSwing(swing) {
+		console.log(swing);
+		if (this.hasLiked(swing.$key)) {
+			swing.likes--;
+		} else {
+			swing.likes++;
+		}
+		this.toggleLike(swing.$key);
+
+	}
 
 }
